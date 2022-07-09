a875fc0c597a967c0fe0c22277b8740ee80844ed
Lukasz <lukasz.sromek@gmail.com>
2022-07-09 15:32:41 +0200

    basic layout

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,12 @@ class MyFrame : public wxFrame
 public:
     MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
     virtual ~MyFrame() {}
+
+private:
+    wxButton *button;
+    wxButton *otherButton;
+
+    void OnButtonClick(wxCommandEvent &e);
 };
 
 wxIMPLEMENT_APP(MyApp);
@@ -24,5 +30,29 @@ bool MyApp::OnInit()
 
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
+{
+    wxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+    wxSizer *centeringSizer = new wxBoxSizer(wxHORIZONTAL);
+
+    wxPanel *panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, this->FromDIP(wxSize(320, 240)));
+    wxSizer *panelSizer = new wxBoxSizer(wxVERTICAL);
+
+    button = new wxButton(panel, wxID_ANY, "Start");
+    button->Bind(wxEVT_BUTTON, &MyFrame::OnButtonClick, this);
+
+    otherButton = new wxButton(panel, wxID_ANY, "Dummy Button");
+
+    panelSizer->Add(button, 0, wxALIGN_CENTER | wxALL, this->FromDIP(5));
+    panelSizer->Add(otherButton, 0, wxALIGN_CENTER | wxBOTTOM, this->FromDIP(20));
+
+    panel->SetSizer(panelSizer);
+
+    centeringSizer->Add(panel, 0, wxALIGN_CENTER);
+    sizer->Add(centeringSizer, 1, wxALIGN_CENTER);
+
+    this->SetSizerAndFit(sizer);
+}
+
+void MyFrame::OnButtonClick(wxCommandEvent &e)
 {
 }
